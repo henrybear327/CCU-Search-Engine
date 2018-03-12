f029b516a10744f269eb36c810e6a5557e6f5b00
Henry Tseng <henrybear327@gmail.com>
2018-03-12 11:03:55 +0800

    Finish prototype, thanks Gary for Bootstrap 4 html source code

---
+++ assignment/1/site/main.py
@@ -0,0 +1,46 @@
+from flask import Flask, render_template, g, request
+from elasticsearch import Elasticsearch
+# import os
+
+app = Flask(__name__)
+
+
+# app.config.from_object(__name__)
+# app.config.update(dict(
+#     DATABASE=os.path.join(app.root_path, 'code/mining.db'),
+# ))
+# app.config['DATABASE']
+
+@app.route('/query', methods=['POST'])
+def query():
+    print(request.form)
+
+    es = Elasticsearch([{'host': 'localhost', 'port': 9200}])
+
+    # get result based on exact match
+    res = es.search(index="ettoday", body={
+        "query": {
+            "match": {
+                "body": request.form['queryString']
+            }
+        }
+    })
+
+    # print(res)
+    data = []
+    for record in res['hits']['hits']:
+        print(record)
+        data.append(record)
+    # print(json.dumps(res, sort_keys=True, indent=4))
+
+    return render_template('index.html', data=data, searchString=request.form['queryString'])
+
+
+@app.route('/')
+def homepage():
+    print(app.root_path)
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)  # development
