6c0193f9a273c45aa8a4845eb7c9d3c728e724fc
Henry Tseng <henrybear327@gmail.com>
2018-03-13 09:43:17 +0800

    Add pagination

--- assignment/1/site/main.py
+++ assignment/1/site/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, g, request
+from flask import Flask, render_template, url_for, request, session, redirect
 from elasticsearch import Elasticsearch
 
 # import os
@@ -12,36 +12,64 @@ app = Flask(__name__)
 # ))
 # app.config['DATABASE']
 
+@app.route('/test/<int:ha>', methods=['GET'])
+def test(ha):
+    return "fuck" + str(ha)
+
+
+@app.route('/test', methods=['POST'])
+def test1():
+    return "POST FUCK"
+
+
 @app.route('/query', methods=['POST'])
-def query():
-    print(request.form)
+@app.route('/query/<int:page>', methods=['GET'])
+def query(page=1):
+    # print(request.form)
 
     es = Elasticsearch([{'host': 'localhost', 'port': 9200}])
+
+    if request.method == 'GET':
+        if session.get('queryString') is None:  # special case
+            return redirect(url_for('homepage'))
+    else:  # POST
+        session['queryString'] = request.form['queryString']
+
+    if page < 1:
+        page = 1
+    if page > 10:
+        page = 10
+    param_from = (page - 1) * 10
+
+    # print(session)
 
     # get result based on exact match
     res = es.search(index="ettoday", body={
         "query": {
             "match": {
-                # "title": request.form['queryString']
-                "body": request.form['queryString']
+                # "title": session['queryString']
+                "body": session['queryString']
             }
         }
-    })
+    }, from_=param_from, size=5)
 
     # print(res)
     data = []
     for record in res['hits']['hits']:
-        print(record)
-        record['_source']['title'] = str(record['_source']['title']).replace(request.form['queryString'],
-                                                                           "<mark>" + request.form[
-                                                                               'queryString'] + "</mark>")
+        # print(record)
 
-        record['_source']['body'] = str(record['_source']['body']).replace(request.form['queryString'],
-                                                                             "<mark>" + request.form['queryString'] + "</mark>")
+        # highlight words that was search
+        record['_source']['title'] = str(record['_source']['title']).replace(session['queryString'],
+                                                                             "<mark>" + session[
+                                                                                 'queryString'] + "</mark>")
+
+        record['_source']['body'] = str(record['_source']['body']).replace(session['queryString'],
+                                                                           "<mark>" + session[
+                                                                               'queryString'] + "</mark>")
         data.append(record)
     # print(json.dumps(res, sort_keys=True, indent=4))
 
-    return render_template('index.html', data=data, searchString=request.form['queryString'])
+    return render_template('index.html', data=data, searchString=session['queryString'], page=page)
 
 
 @app.route('/')
@@ -51,4 +79,6 @@ def homepage():
 
 
 if __name__ == '__main__':
+    app.secret_key = 'super secret key'
+    # app.config['SESSION_TYPE'] = 'filesystem'
     app.run(debug=True)  # development
