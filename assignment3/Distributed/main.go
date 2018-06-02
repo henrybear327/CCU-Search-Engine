ff81c7f1a4a112c35fea871101ed32b49c4117e0
Henry Tseng <henrybear327@gmail.com>
2018-06-02 23:09:48 +0800

    update master slave code

--- assignment3/Distributed/main.go
+++ assignment3/Distributed/main.go
@@ -1,5 +1,5 @@
 // netstat -vanp tcp | grep -e "50507"
-// source code from https://confusedcoders.com/go/create-a-basic-distributed-system-in-go-lang-part-1
+// modified from https://confusedcoders.com/go/create-a-basic-distributed-system-in-go-lang-part-1
 
 package main
 
@@ -7,135 +7,163 @@ import (
 	"encoding/json"
 	"flag"
 	"fmt"
+	"log"
 	"math/rand"
 	"net"
 	"strconv"
-	"strings"
 	"time"
 )
 
-/* Information/Metadata about node */
+// NodeInfo is the Information/Metadata about node
 type NodeInfo struct {
-	NodeId     int    `json:"nodeId"`
-	NodeIpAddr string `json:"nodeIpAddr"`
+	NodeID     int    `json:"nodeID"`
+	NodeIPAddr string `json:"nodeIPAddr"`
 	Port       string `json:"port"`
 }
 
-/* A standard format for a Request/Response for adding node to cluster */
+// Just for pretty printing the node info
+func (node NodeInfo) String() string {
+	return "NodeInfo: {\n\tnodeID:" + strconv.Itoa(node.NodeID) + ", \n\tnodeIPAddr:" + node.NodeIPAddr + ", \n\tport:" + node.Port + " \n}"
+}
+
+// AddToClusterMessage is a standard format for a Request/Response for adding node to cluster
 type AddToClusterMessage struct {
 	Source  NodeInfo `json:"source"`
 	Dest    NodeInfo `json:"dest"`
 	Message string   `json:"message"`
 }
 
-/* Just for pretty printing the node info */
-func (node NodeInfo) String() string {
-	return "NodeInfo:{ nodeId:" + strconv.Itoa(node.NodeId) + ", nodeIpAddr:" + node.NodeIpAddr + ", port:" + node.Port + " }"
-}
-
-/* Just for pretty printing Request/Response info */
+// Just for pretty printing Request/Response info
 func (req AddToClusterMessage) String() string {
-	return "AddToClusterMessage:{\n  source:" + req.Source.String() + ",\n  dest: " + req.Dest.String() + ",\n  message:" + req.Message + " }"
+	return "AddToClusterMessage:{\nsource:" + req.Source.String() + ",\ndest: " + req.Dest.String() + ",\nmessage:" + req.Message + " }"
 }
 
-/* The entry point for our System */
-func main() {
-	/* Parse the provided parameters on command line */
+// praseCommandLine parses the provided parameters on command line
+func praseCommandLine() (bool, string, string) {
 	makeMasterOnError := flag.Bool("makeMasterOnError", false, "make this node master if unable to connect to the cluster ip provided.")
 	clusterip := flag.String("clusterip", "127.0.0.1:8001", "ip address of any node to connnect")
 	myport := flag.String("myport", "8001", "ip address to run this node on. default is 8001.")
 	flag.Parse()
 
-	/* Generate id for myself */
-	rand.Seed(time.Now().UTC().UnixNano())
-	myid := rand.Intn(99999999)
+	return *makeMasterOnError, *clusterip, *myport
+}
 
-	myIp, _ := net.InterfaceAddrs()
-	me := NodeInfo{NodeId: myid, NodeIpAddr: myIp[0].String(), Port: *myport}
-	dest := NodeInfo{NodeId: -1, NodeIpAddr: strings.Split(*clusterip, ":")[0], Port: strings.Split(*clusterip, ":")[1]}
-	fmt.Println("My details:", me.String())
-
-	/* Try to connect to the cluster, and send request to cluster if able to connect */
-	ableToConnect := connectToCluster(me, dest)
-
-	/*
-	 * Listen for other incoming requests form other nodes to join cluster
-	 * Note: We are not doing anything fancy right now to make this node as master. Not yet!
-	 */
-	if ableToConnect || (!ableToConnect && *makeMasterOnError) {
-		if *makeMasterOnError {
-			fmt.Println("Will start this node as master.")
-		}
-		listenOnPort(me)
-	} else {
-		fmt.Println("Quitting system. Set makeMasterOnError flag to make the node master.", myid)
+func check(info string, err error) {
+	if err != nil {
+		log.Fatalln("error on", info, ":", err)
 	}
 }
 
-/*
- * This is a useful utility to format the json packet to send requests
- * This tiny block is sort of important else you will end up sending blank messages.
- */
-func getAddToClusterMessage(source NodeInfo, dest NodeInfo, message string) AddToClusterMessage {
+func setupNodes(clusterip, myport string) (int, NodeInfo, NodeInfo) {
+	/* Generate id for myself */
+	rand.Seed(time.Now().UTC().UnixNano())
+	myID := rand.Intn(99999999)
+
+	myIP, err := net.InterfaceAddrs()
+	check("net.InterfaceAddrs()", err)
+	me := NodeInfo{
+		NodeID:     myID,
+		NodeIPAddr: myIP[0].String(),
+		Port:       myport,
+	}
+
+	destHost, destPort, err := net.SplitHostPort(clusterip)
+	if err != nil {
+		log.Fatal("net.SplitHostPort", err)
+	}
+	// log.Println("dest IP", destHost, "dest port", destPort)
+
+	dest := NodeInfo{
+		NodeID:     -1,
+		NodeIPAddr: destHost,
+		Port:       destPort,
+	}
+	log.Println("My details: ", me.String())
+
+	return myID, me, dest
+}
+
+func main() {
+	makeMasterOnError, clusterip, myport := praseCommandLine()
+
+	myID, me, dest := setupNodes(clusterip, myport)
+
+	ableToConnect := connectToCluster(me, dest)
+
+	if ableToConnect || (!ableToConnect && makeMasterOnError) {
+		if makeMasterOnError {
+			log.Println("Will start this node as master.")
+			me.NodeID = 0 // master ID = 0
+		}
+
+		listenOnPort(me)
+	} else {
+		log.Println("Quitting system. Set makeMasterOnError flag to make the node master.", myID)
+	}
+}
+
+func getAddToClusterMessage(source, dest NodeInfo, message string) AddToClusterMessage {
 	return AddToClusterMessage{
 		Source: NodeInfo{
-			NodeId:     source.NodeId,
-			NodeIpAddr: source.NodeIpAddr,
+			NodeID:     source.NodeID,
+			NodeIPAddr: source.NodeIPAddr,
 			Port:       source.Port,
 		},
 		Dest: NodeInfo{
-			NodeId:     dest.NodeId,
-			NodeIpAddr: dest.NodeIpAddr,
+			NodeID:     dest.NodeID,
+			NodeIPAddr: dest.NodeIPAddr,
 			Port:       dest.Port,
 		},
 		Message: message,
 	}
 }
 
-func connectToCluster(me NodeInfo, dest NodeInfo) bool {
+func connectToCluster(me, dest NodeInfo) bool {
 	/* connect to this socket details provided */
-	connOut, err := net.DialTimeout("tcp", dest.NodeIpAddr+":"+dest.Port, time.Duration(10)*time.Second)
+	connOut, err := net.DialTimeout("tcp", net.JoinHostPort(dest.NodeIPAddr, dest.Port), time.Duration(10)*time.Second)
 	if err != nil {
-		if _, ok := err.(net.Error); ok {
-			fmt.Println("Couldn't connect to cluster.", me.NodeId)
-			return false
-		}
-	} else {
-		fmt.Println("Connected to cluster. Sending message to node.")
-		text := "Hi nody! please add me to the cluster."
-		requestMessage := getAddToClusterMessage(me, dest, text)
-		json.NewEncoder(connOut).Encode(&requestMessage)
-
-		decoder := json.NewDecoder(connOut)
-		var responseMessage AddToClusterMessage
-		decoder.Decode(&responseMessage)
-		fmt.Println("Got response:\n" + responseMessage.String())
-
-		return true
+		log.Println("Couldn't connect to cluster.", me.NodeID)
+		return false
 	}
-	return false
+
+	log.Println("Connected to master. Sending a message to master.")
+	text := "Please add node " + strconv.Itoa(me.NodeID) + " to master"
+	requestMessage := getAddToClusterMessage(me, dest, text)
+	json.NewEncoder(connOut).Encode(&requestMessage)
+
+	decoder := json.NewDecoder(connOut)
+	var responseMessage AddToClusterMessage
+	decoder.Decode(&responseMessage)
+	log.Println("Got response:\n" + responseMessage.String())
+
+	return true
+}
+
+func handleConnection(connIn net.Conn, me NodeInfo) {
+	var requestMessage AddToClusterMessage
+	json.NewDecoder(connIn).Decode(&requestMessage)
+	log.Println("Master got request:\n" + requestMessage.String())
+
+	text := "Master received add slave request from " + strconv.Itoa(requestMessage.Source.NodeID) + " and added it"
+	responseMessage := getAddToClusterMessage(me, requestMessage.Source, text)
+	json.NewEncoder(connIn).Encode(&responseMessage)
+	connIn.Close()
 }
 
 func listenOnPort(me NodeInfo) {
 	/* Listen for incoming messages */
-	ln, _ := net.Listen("tcp", fmt.Sprint(":"+me.Port))
+	ln, err := net.Listen("tcp", fmt.Sprint(":"+me.Port))
+	check("net.Listen", err)
+	defer ln.Close()
 	/* accept connection on port */
 	/* not sure if looping infinetely on ln.Accept() is good idea */
 	for {
 		connIn, err := ln.Accept()
 		if err != nil {
-			if _, ok := err.(net.Error); ok {
-				fmt.Println("Error received while listening.", me.NodeId)
-			}
-		} else {
-			var requestMessage AddToClusterMessage
-			json.NewDecoder(connIn).Decode(&requestMessage)
-			fmt.Println("Got request:\n" + requestMessage.String())
-
-			text := "Sure buddy! Too easy."
-			responseMessage := getAddToClusterMessage(me, requestMessage.Source, text)
-			json.NewEncoder(connIn).Encode(&responseMessage)
-			connIn.Close()
+			log.Println("Error received while listening.", me.NodeID)
+			continue
 		}
+
+		go handleConnection(connIn, me)
 	}
 }
